432245d4fe11a0d983b22aef9142a5706d52b7db
Chirane <e6296083@cmaisonneuve.qc.ca>
2025-04-16 10:47:26 -0400

    Ajout des fichier du tp

---
+++ src/pyforum/commentaire.py
@@ -0,0 +1,13 @@
+class Commentaire():
+
+    def __init__(self, id, id_auteur, contenu_commentaire, id_publication):
+
+        # TODO: Ajouter les autres attributs nécessaires
+        self.id = id
+        self.id_auteur = id_auteur
+        self.contenu_commentaire = contenu_commentaire
+        self.id_publication = id_publication
+    
+    def __str__(self):
+        return f"Commentaire(id={self.id}, id_auteur='{self.id_auteur}', contenu_commentaire='{self.contenu_commentaire}', id_publication='{self.id_publication}')"
+        
--- src/pyforum/forum.py
+++ src/pyforum/forum.py
@@ -1,14 +1,12 @@
-class Utilisateur():
+class Forum():
 
-    def __init__(self, id, username, email, password, liste_publications):
+    def __init__(self, id, username, description,liste_publications):
         # TODO: Ajouter les autres attributs nécessaires
         self.id = id
         self.username = username
-        self.email = email
-        self.password = password
+        self.description = description
         self.liste_publications = liste_publications
 
     def __str__(self):
         return f"Utilisateur(id={self.id}, username='{self.username}')"
-    
     
