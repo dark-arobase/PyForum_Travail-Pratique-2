cbc774b1ca5411a31580f0a451fd1cfcaa8a22b1
s3nkumega <75550482+s3nkumega@users.noreply.github.com>
2025-04-18 15:21:56 -0400

    modification du code de la class commentaire

--- src/pyforum/commentaire.py
+++ src/pyforum/commentaire.py
@@ -1,16 +1,9 @@
-class Commentaire():
-
-    def __init__(self, id, id_auteur, contenu_commentaire, id_publication):
-
-        # TODO: Ajouter les autres attributs nécessaires
+class Commentaire:
+    def __init__(self, id, id_auteur, contenu, id_publication):
         self.id = id
         self.id_auteur = id_auteur
-        self.contenu_commentaire = contenu_commentaire
+        self.contenu = contenu
         self.id_publication = id_publication
-    
+
     def __str__(self):
-        return f"Commentaire(id={self.id}, id_auteur='{self.id_auteur}', contenu_commentaire='{self.contenu_commentaire}', id_publication='{self.id_publication}')"
-        
-        
-    def __str__(self):
-        return f"Commentaire de {self.auteur_id} : {self.contenu}"
+        return f"Commentaire({self.id}, auteur={self.id_auteur})"
