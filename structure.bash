cdbca00fab9e7b592ef213a192badf4addee3eba
s3nkumega <75550482+s3nkumega@users.noreply.github.com>
2025-04-18 16:08:07 -0400

    ajout d une structure

---
+++ structure.bash
@@ -0,0 +1,23 @@
+PyForum_Complet/
+├── data/                            
+│   ├── utilisateur.csv              
+│   ├── forum.csv                    
+│   ├── publications.json            
+│   ├── commentaires.json            
+│   └── rejoindre_forum.csv          
+├── src/
+│   └── pyforum/
+│       ├── __main__.py
+│       ├── __init__.py
+│       ├── bd.py
+│       ├── mvp.py
+│       ├── utilisateur.py
+│       ├── forum.py
+│       ├── publication.py
+│       ├── commentaire.py
+│       └── __pycache__/
+├── .gitignore
+├── LICENSE
+├── pyproject.toml
+├── README.md
+└── structure.bash
