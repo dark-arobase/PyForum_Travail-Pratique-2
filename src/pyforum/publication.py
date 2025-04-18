dea0ed8179b1ac9b3f5ce68c59f76c6761f92361
s3nkumega <75550482+s3nkumega@users.noreply.github.com>
2025-04-18 15:28:28 -0400

    ajustement du code

--- src/pyforum/publication.py
+++ src/pyforum/publication.py
@@ -1,15 +1,12 @@
-class Publication():
-
-    def __init__(self, id, titre, contenu,  date_creation, id_auteur, id_forum, liste_commentaires):
-
-        # TODO: Ajouter les autres attributs nécessaires
+class Publication:
+    def __init__(self, id, titre, contenu, date_creation, id_auteur, id_forum):
         self.id = id
         self.titre = titre
         self.contenu = contenu
         self.date_creation = date_creation
         self.id_auteur = id_auteur
         self.id_forum = id_forum
-        self.liste_commentaires = liste_commentaires
-    
+        self.commentaires = []
+
     def __str__(self):
-        return f"Publication(id={self.id}, titre='{self.titre}', contenu='{self.contenu}', date_creation='{self.date_creation}', id_auteur='{self.id_auteur}', id_forum='{self.id_forum}')"
+        return f"Publication({self.id}, {self.titre})"
