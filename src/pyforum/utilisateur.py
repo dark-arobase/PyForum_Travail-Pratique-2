1cfd102a2d1a5c95b48d930b4629763f60fc5eea
massyle <mchirane@gmail.com>
2025-04-25 23:33:20 -0400

    modif class utilisateur et ajout liste_forums

--- src/pyforum/utilisateur.py
+++ src/pyforum/utilisateur.py
@@ -1,10 +1,10 @@
 class Utilisateur:
-    def __init__(self, id, username, courriel, mot_de_passe):
+    def __init__(self, id, username, courriel, mot_de_passe, liste_forums=None):
         self.id = id
         self.username = username
         self.courriel = courriel
         self.mot_de_passe = mot_de_passe
-        self.forums = []
+        self.forums = liste_forums if liste_forums is not None else []
 
     def __str__(self):
         return f"Utilisateur({self.id}, {self.username})"
